dbd73b3dfbede6541ddca7928778a6efdf34a3bd
Irakli Javaxadze <rkljavaxa@gmail.com>
2024-12-12 14:28:33 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,8 +30,8 @@ const coaFiles = {
     "COA_RE21096417.pdf": "https://main--stellular-khapse-e51f2d.netlify.app/coas/COA_RE21096417.pdf",
     "COA_RE21095480.pdf": "https://main--stellular-khapse-e51f2d.netlify.app/coas/COA_RE21095480.pdf",
     "COA_RE21096266.pdf": "https://main--stellular-khapse-e51f2d.netlify.app/coas/COA_RE21096266.pdf",
-    "Hazen_COA_Set_25.pdf": "https://main--stellular-khapse-e51f2d.netlify.app/coas/Hazen_COA_Set_25.pdf",
-    "COA_RE21096307.pdf": "https://main--stellular-khapse-e51f2d.netlify.app/coas/COA_RE21096307.pdf"
+    "COA_RE21096307.pdf": "https://main--stellular-khapse-e51f2d.netlify.app/coas/COA_RE21096307.pdf",
+    "Hazen_COA_Set_25.pdf": "https://main--stellular-khapse-e51f2d.netlify.app/coas/Hazen_COA_Set_25.pdf"
 };
 
     let sampleData = [];
